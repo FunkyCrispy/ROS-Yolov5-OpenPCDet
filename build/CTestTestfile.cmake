4bf70b3871f897abaf691e310a7e1f099aaa6fa9
FunkyCrispy <chenf_funky@163.com>
2021-12-28 14:32:13 +0800

    new

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fengchen/projects/ros-yolov5-pointpillar/src
-# Build directory: /home/fengchen/projects/ros-yolov5-pointpillar/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(rospy_sensorfusion)
--- build/rospy_sensorfusion/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fengchen/projects/ros-yolov5-pointpillar/src/rospy_sensorfusion
-# Build directory: /home/fengchen/projects/ros-yolov5-pointpillar/build/rospy_sensorfusion
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
